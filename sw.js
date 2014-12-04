5ee669d419fe2d51e7da935a032b466235a39d1c
Marius Gundersen <Marius.Gundersen@ciber.com>
2014-12-04 13:21:08 +0100

    initial commit

---
+++ sw.js
@@ -0,0 +1,11 @@
+// The files we want to cache
+var urlsToCache = [
+  '/index.html',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+// Set the callback for the install step
+self.addEventListener('install', function(event) {
+    // Perform install steps
+});
